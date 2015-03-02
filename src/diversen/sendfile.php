3854040c1de236bccc05e3a7b5213aa66ff139db
Dennis Iversen <dennis.iversen@gmail.com>
2015-03-02 17:09:58 +0100

    add source

---
+++ src/diversen/sendfile.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace diversen;
+
+/**
+ * A terribly useful class
+ *
+ */
+class sendfile
+{
+    //public 
+    /**
+     * if false we set content disposition from file that will be sent
+     * @var mixed $disposition    
+     */
+    public $disposition = false;
+    
+    /**
+     * throttle speed in secounds
+     * @var float $sec
+     */
+    public $sec = 0.1;
+    
+    /**
+     * bytes per $sec
+     * @var int $bytes 
+     */
+    public $bytes = 40960;
+    
+    /**
+     * if contentType is false we try to guess it
+     * @var mixed $contentType 
+     */
+    public $type = false;
+    
+    /**
+     * set content disposition 
+     * @param type $file_name
+     */
+    public function contentDisposition ($file_name = false) {
+        $this->disposition = $file_name;
+    }
+    
+    /**
+     * set throttle speed
+     * @param float $sec
+     * @param int $bytes
+     */
+    public function throttle ($sec = 0.1, $bytes = 40960) {
+        $this->sec = $sec;
+        $this->bytes = $bytes;
+    }
+    
+    /**
+     * set content mime type if false we try to guess it
+     * @param string $content_type
+     */
+    public function contentType ($content_type = null) {
+        $this->type = $content_type;
+    }
+
+    /**
+     * get name from path info
+     * @param type $file
+     * @return type
+     */
+    public function name ($file) {
+        $info = pathinfo($file);
+        return $info['basename'];
+        
+    }
+
+    /**
+     * file to send
+     * @param type $file_path
+     * @param type $disposition
+     * @param type $type
+     * @throws Exception
+     */
+    public function send($file_path) {
+        
+        if (!is_readable($file_path)) {
+            throw new Exception('File not found or inaccessible!');
+        }
+
+        $size = filesize($file_path);
+        if (!$this->disposition) {
+            $this->disposition = $this->name($file_path);
+        }
+        
+        if (!$this->type) {
+            $this->type = $this->getContentType($file_path);
+        }
+
+        //turn off output buffering to decrease cpu usage
+        $this->cleanAll();
+        
+        // required for IE, otherwise Content-Disposition may be ignored
+        if (ini_get('zlib.output_compression')) {
+            ini_set('zlib.output_compression', 'Off');
+        }
+
+        header('Content-Type: ' . $this->type);
+        header('Content-Disposition: attachment; filename="' . $this->disposition . '"');
+        header("Content-Transfer-Encoding: binary");
+        header('Accept-Ranges: bytes');
+
+        // The three lines below basically make the
+        // download non-cacheable 
+        header("Cache-control: private");
+        header('Pragma: private');
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+
+        // multipart-download and download resuming support
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            list($a, $range) = explode("=", $_SERVER['HTTP_RANGE'], 2);
+            list($range) = explode(",", $range, 2);
+            list($range, $range_end) = explode("-", $range);
+            $range = intval($range);
+            if (!$range_end) {
+                $range_end = $size - 1;
+            } else {
+                $range_end = intval($range_end);
+            }
+
+            $new_length = $range_end - $range + 1;
+            header("HTTP/1.1 206 Partial Content");
+            header("Content-Length: $new_length");
+            header("Content-Range: bytes $range-$range_end/$size");
+        } else {
+            $new_length = $size;
+            header("Content-Length: " . $size);
+        }
+
+        /* output the file itself */
+        $chunksize = $this->bytes; //you may want to change this
+        $bytes_send = 0;
+        
+        $file = @fopen($file_path, 'r');
+        if ($file) {
+            if (isset($_SERVER['HTTP_RANGE'])) {
+                fseek($file, $range);
+            }
+
+            while (!feof($file) && (!connection_aborted()) && ($bytes_send < $new_length) ) {
+                $buffer = fread($file, $chunksize);
+                echo($buffer); //echo($buffer); // is also possible
+                flush();
+                usleep($this->sec * 1000000);
+                $bytes_send += strlen($buffer);
+            }
+            fclose($file);
+        } else
+            die('Error - can not open file.');
+
+        die();
+    }
+    
+    /**
+     * method for getting mime type of a file
+     * @param string $path
+     * @return string $mime_type 
+     */
+    public function getContentType($path) {
+        $result = false;
+        if (is_file($path) === true) {
+            if (function_exists('finfo_open') === true) {
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                if (is_resource($finfo) === true) {
+                    $result = finfo_file($finfo, $path);
+                }
+                finfo_close($finfo);
+            } else if (function_exists('mime_content_type') === true) {
+                $result = preg_replace('~^(.+);.*$~', '$1', mime_content_type($path));
+            } else if (function_exists('exif_imagetype') === true) {
+                $result = image_type_to_mime_type(exif_imagetype($path));
+            }
+        }
+        return $result;
+    }
+    
+    public function cleanAll() {
+
+        while (ob_get_level()) {
+            ob_end_flush();
+        }
+    }
+}
+
