78ccfbe05a304aec00a41dc1cad21ac805af5e12
Dennis Iversen <dennis.iversen@gmail.com>
2015-03-02 19:57:00 +0100

    private and public

---
+++ src/diversen/HalloThere.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace diversen\sendfile;
+
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class HalloThere
+{
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
